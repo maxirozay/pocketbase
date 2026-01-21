57a0e99c4ae1f02d72d510d5b8d52f14218a2bb4
Maxime Rossier <rossier.maxime@gmail.com>
2026-01-21 16:52:51 +0100

    upload files

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,29 @@ const pb = new PocketBase('http://127.0.0.1:8090')
 pb.collection('files').getList(1, 20, {
   filter: 'created > "2022-08-01 10:00:00"'
 }).then((result) => console.log(result))
+
+const uploadFile = async (event: Event) => {
+  const input = event.target as HTMLInputElement
+  if (!input.files?.length) return
+
+  try {
+    const formData = new FormData()
+    formData.append('file', input.files[0]!)
+    for (let file of input.files) {
+      formData.append('files', file);
+    }
+    
+    const record = await pb.collection('files').create(formData)
+    console.log('File uploaded:', record)
+  } catch (err) {
+    console.error('Upload failed:', err)
+  }
+}
 </script>
 
 <template>
   <h1>You did it!</h1>
+  <input type="file" multiple @change="uploadFile" />
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation

ac5eeb2b854257853a2ffef4b801e26bc30e08e7
Maxime Rossier <rossier.maxime@gmail.com>
2026-02-03 15:58:15 +0100

    deploy and update binary

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,23 @@ npm run build
 # Use rsync to only upload files that have changed
 rsync -av -e "ssh -i $SSH_KEY" pocketbase/pb_migrations $SERVER_URL:$REMOTE_PATH/
 rsync -av -e "ssh -i $SSH_KEY" pocketbase/pb_public $SERVER_URL:$REMOTE_PATH/
+rsync -av -e "ssh -i $SSH_KEY" pocketbase/pb_hooks $SERVER_URL:$REMOTE_PATH/
 
 # Clean up files in pb_public older than 30 days to save space
 ssh -i $SSH_KEY $SERVER_URL "find $REMOTE_PATH/pb_public -type f -mtime +30 -delete"
 
+if [ "$1" = "--pb" ]; then
+  echo "Uploading new PocketBase binary..."
+  scp -i $SSH_KEY pocketbase/pocketbase_server $SERVER_URL:$REMOTE_PATH/pocketbase_new
+  ssh -i $SSH_KEY $SERVER_URL "chmod +x $REMOTE_PATH/pocketbase_new"
+
+  echo "Restarting service..."
+  ssh -i $SSH_KEY $SERVER_URL "sudo systemctl stop pocketbase && mv $REMOTE_PATH/pocketbase_new $REMOTE_PATH/pocketbase && sudo systemctl start pocketbase"
+  echo "PocketBase binary updated and service restarted."
+else
+  echo "Skipping binary update. Run with --pb to update the server binary."
+fi
 
 ## scp alternative (uncomment if you prefer scp over rsync)
 # scp -r -i $SSH_KEY pocketbase/pb_migrations $SERVER_URL:$REMOTE_PATH/pb_migrations
 # scp -r -i $SSH_KEY pocketbase/pb_public $SERVER_URL:$REMOTE_PATH/pb_public
-
-# scp -i $SSH_KEY pocketbase/pocketbase $SERVER_URL:$REMOTE_PATH/pocketbase # deploy pocketbase binary if needed (make sure it's built for the right architecture)
